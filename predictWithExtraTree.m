438b1219645dd01b8f9ede9e626d78f793babbda
Riccardo Taormina <riccardo.taormina@gmail.com>
2016-03-16 17:55:44 +0800

    Substituted Foobar with MATLAB_ExtraTrees

--- predictWithExtraTree.m
+++ predictWithExtraTree.m
@@ -31,7 +31,7 @@ function [ output ] = predictWithExtraTree(TREE,S,problemType)
 %     the Free Software Foundation, either version 3 of the License, or
 %     (at your option) any later version.
 % 
-%     Foobar is distributed in the hope that it will be useful,
+%     MATLAB_ExtraTrees is distributed in the hope that it will be useful,
 %     but WITHOUT ANY WARRANTY; without even the implied warranty of
 %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %     GNU General Public License for more details.
